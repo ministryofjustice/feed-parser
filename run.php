f297a9f2e840cf16d10e28e5c5e56e0f176793c2
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-08-09 13:20:11 +0100

    Add Feed array to allow us to handle multiple feeds

--- run.php
+++ run.php
@@ -7,32 +7,30 @@ require 'inc/OleeoFeedParser.php';
 use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
 
-// Fetch the XML feed using wget
-exec('wget -O /output/input.xml https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-2/candidate/jobboard/vacancy/3/feed/structured');
+$feeds = [
+    [
+        'id' => 'moj-oleeo-structured',
+        'name' => 'MOJ Oleeo Jobs Structured Feed',
+        'url' => 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-2/candidate/jobboard/vacancy/3/feed/structured',
+        'type' => 'complex'
+    ],
+    [
+        'id' => 'moj-oleeo-simple',
+        'name' => 'MOJ Oleeo Jobs Simple Feed',
+        'url' => 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-2/candidate/jobboard/vacancy/3/feed',
+        'type' => 'simple'
+    ]
+ ];
 
-$feed_parser = new OleeoFeedParser();
+ if(count($feeds) == 0){
+    return;
+ }
 
-$optionalFields = [
-    'id',
-    'closingDate',
-    'salary',
-    'addresses',
-    'cities',
-    'regions',
-    'roleTypes',
-    'contractTypes'
-];
-
-$feed_parser->parseToJSON('output/input.xml', 'output/structured-feed.json', $optionalFields);
-
-// AWS S3 Bucket Name
+ // AWS S3 Bucket Name
 $s3BucketName = getenv('S3_UPLOADS_BUCKET');
 
 // AWS S3 Bucket Path
 $s3BucketPath = '';  // Set to an empty string if the root of the bucket
-
-// AWS S3 Object Key
-$s3ObjectKey = $s3BucketPath . 'structured-feed.json';
 
 // AWS S3 Region
 $awsRegion = 'eu-west-2';
@@ -43,18 +41,58 @@ $s3Client = new S3Client([
     'version' => 'latest'
 ]);
 
-// Upload to AWS s3 bucket
-try {
-    // Upload the file to S3 bucket
-    $result = $s3Client->putObject([
-        'Bucket' => $s3BucketName,
-        'Key' => $s3ObjectKey,
-        'ACL' => 'public-read',
-        'SourceFile' => '/output/structured-feed.json'
-    ]);
 
-    $uploadedFileName = basename('/output/structured-feed.json');
-    echo "File '$uploadedFileName' uploaded to S3 successfully." . PHP_EOL;
-} catch (AwsException $e) {
-    echo 'Error: ' . $e->getMessage() . PHP_EOL;
-}
+ foreach ($feed as $feed){
+
+    $feedID = $feed['id'];
+    $feedURL = $feed['url'];
+    $xmlName = "$feedID.xml";
+    $xmlLocation = "/output/$feedName";
+
+    // Fetch the XML feed using wget
+    exec("wget -O  $xmlLocation $feedURL");
+
+    $feed_parser = new OleeoFeedParser();
+
+    $optionalFields = [
+        'id',
+        'closingDate',
+        'salary',
+        'addresses',
+        'cities',
+        'regions',
+        'roleTypes',
+        'contractTypes'
+    ];
+
+
+    $jsonFile = "output/$feedID.json";
+
+    $result = $feed_parser->parseToJSON("output/$xmlName", $jsonFile, $optionalFields, $feed['type']);
+
+    if(!$result['success']){
+        return;
+    }
+
+    // AWS S3 Object Key
+    $s3ObjectKey = $s3BucketPath . "$feedID.json";
+
+    // Upload to AWS s3 bucket
+    try {
+        // Upload the file to S3 bucket
+        $result = $s3Client->putObject([
+            'Bucket' => $s3BucketName,
+            'Key' => $s3ObjectKey,
+            'ACL' => 'public-read',
+            'SourceFile' => '/' . $jsonFile
+        ]);
+
+        $uploadedFileName = basename('/' . $jsonFile);
+        echo "File '$uploadedFileName' uploaded to S3 successfully." . PHP_EOL;
+    } catch (AwsException $e) {
+        echo 'Error: ' . $e->getMessage() . PHP_EOL;
+    }
+ }
+
+
+
